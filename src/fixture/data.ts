febd1028f0317ed25bc9927c9b6493751b88d9ff
Salomon Moreno <msalomon@vmware.com>
2024-08-11 08:48:48 -0500

    Modify README and add fixture data

---
+++ src/fixture/data.ts
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    description: "File 2023 Taxes",
+    isComplete: true,
+    dueDate: "2023-03-10T17:50:44.673Z",
+  },
+  {
+    id: 2,
+    description: "Fold laundry",
+    isComplete: true,
+    dueDate: null,
+  },
+  {
+    id: 3,
+    description: "Call Mom",
+    isComplete: false,
+    dueDate: "2023-06-26T19:00:00.000Z",
+  },
+  {
+    id: 4,
+    description: "Walk the dog",
+    isComplete: false,
+    dueDate: null,
+  },
+  {
+    id: 5,
+    description: "Feed the cat",
+    isComplete: false,
+    dueDate: "2024-06-24T15:45:00.000Z",
+  },
+  {
+    id: 6,
+    description: "Run LA marathon",
+    isComplete: false,
+    dueDate: "2024-09-21T13:30:00.000Z",
+  },
+];
